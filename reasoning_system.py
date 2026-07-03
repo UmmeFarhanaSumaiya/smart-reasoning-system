804525ce0495095d54bcb0608c5bb10e62d22a1c
ummefarhanasumaiya <farhana23sumaiya@gmail.com>
2026-07-03 23:47:22 +0600

    Add analyze_parts function - Phase 2 complete

--- reasoning_system.py
+++ reasoning_system.py
@@ -5,8 +5,6 @@ from google import genai
 load_dotenv()
 key = os.getenv("GEMINI_API_KEY")
 
-
-
 def decompose_problem(problem):
 
     Client = genai.Client()
@@ -16,6 +14,18 @@ def decompose_problem(problem):
     )
     return(response)
 
+def analyze_parts(problem):
+
+    Client = genai.Client()
+    response = Client.models.generate_content(
+    model="gemini-3.5-flash",
+    contents="Analyze the result into very samaller part. do a deep analyze and explain why:" +problem
+    )
+    return(response)
+
 question = input("Write your complex thought here: ")
 result = decompose_problem(question)
 print(result.text)
+
+result2 = analyze_parts(result.text)
+print(result2.text)
